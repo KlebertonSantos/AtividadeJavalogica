31531e8e136009071dbf7eee8a212ef924db1abb
klebertonSantos <kleberton.ferreira@mercadolivre.com>
2023-01-12 19:08:10 -0300

    commit atividade laçoes repetições 3

--- Atividade2.java
+++ Atividade2.java
@@ -1,27 +1,32 @@
-package Atividadesjava;
+package AtividadeLacosFor3Java;
 
 import java.util.Scanner;
 
 public class Atividade2 {
     public static void main(String[] args) {
-        Scanner ler = new Scanner(System.in);
-        float nota1;
-        float nota2;
-        float nota3;
-        float nota4;
-        float media;
+        int idmaior = 0,idmenor =0, idade, contador = 1;
+        Scanner leia = new Scanner(System.in);
+
+        while (contador < 12) {
+            System.out.println("Digite uma idade " + contador);
+            idade = leia.nextInt();
+             if (idade < 21 && idade > 0){
+                 idmenor = idmenor +1;
+
+            }else if (idade > 50) {
+                 idmaior = idmaior +1;
+                 
+             }
 
 
-        System.out.println("Digite a nota1");
-        nota1 = ler.nextFloat();
-        System.out.println("Digite a nota2");
-        nota2 = ler.nextFloat();
-        System.out.println("Digite a nota3");
-        nota3 = ler.nextFloat();
-        System.out.println("Digite a nota4");
-        nota4 = ler.nextFloat();
 
-        media = (nota1 + nota2 + nota3 + nota4) /4;
-        System.out.printf("Media final é %.1f", Math.abs(media));
+
+            contador++;
+        }
+        System.out.println("Idade menor que 21 anos é :" +idmenor);
+        System.out.println("idade maior que 50 anos é:"+idmaior);
+        System.out.println("\t\t_________Fim do programa_________");
+
     }
+
 }
---
+++ AtividadeGeralLacosRepeticoes3Java.java
@@ -0,0 +1,88 @@
+package AtividadeLacosFor3Java;
+
+import java.util.Scanner;
+
+public class AtividadeGeralLacosRepeticoes3Java {
+    // Atividade 1
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int num, par = 0, impar = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Digite o " + i + " número: ");
+            num = ler.nextInt();
+            if (num % 2 == 0) {
+                par += 1;
+
+
+            } else impar += 1;
+        }
+
+        System.out.println("Total de números pares: " + par + "\n Total de número impares: " + impar);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+         package AtividadeLacosFor3Java;
+
+import java.util.Scanner;
+
+        public class Atividade2 {
+            public static void main(String[] args) {
+                int idmaior = 0,idmenor =0, idade, contador = 1;
+                Scanner leia = new Scanner(System.in);
+
+                while (contador < 12) {
+                    System.out.println("Digite uma idade " + contador);
+                    idade = leia.nextInt();
+                    if (idade < 21 && idade > 0){
+                        idmenor = idmenor +1;
+
+                    }else if (idade > 50) {
+                        idmaior = idmaior +1;
+
+                    }
+
+
+
+
+                    contador++;
+                }
+                System.out.println("Idade menor que 21 anos é :" +idmenor);
+                System.out.println("idade maior que 50 anos é:"+idmaior);
+                System.out.println("\t\t_________Fim do programa_________");
+
+            }
+
+        }
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+       package AtividadeLacosFor3Java;
+
+import java.util.Scanner;
+
+        public class Ativa3 {
+            public static void main(String[] args) {
+                int numero;
+                int soma = 0;
+                int contador = 0;
+                Scanner leia = new Scanner(System.in);
+
+                do {
+                    System.out.println("\nDigite um número: ");
+                    numero = leia.nextInt();
+
+                    if (numero > 0)
+                        soma = soma + numero;
+
+
+
+
+                } while (numero != 0);
+
+                System.out.println("A soma dos números positivos é : "+ soma);
+                contador ++;
+
+            }
+        }
+
+    }
+
+}
